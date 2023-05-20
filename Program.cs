3d9bfaea259b62171de79cd4de97afdfc9353fab
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-20 13:07:30 -0600

    Setup database and services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using visitas_residenciales;
+using visitas_residenciales.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<VisitasResidencialesContext>("Data Source=localhost; Initial Catalog=visitasDB;user id=sa;password=Programaci0n$;Encrypt=False");
+builder.Services.AddScoped<ICasaService, CasaService>();
+builder.Services.AddScoped<IHabitanteService, HabitanteService>();
+builder.Services.AddScoped<IResidenteService, ResidenteService>();
+builder.Services.AddScoped<IVisitaService, VisitaService>();
+builder.Services.AddScoped<IVisitanteService, VisitanteService>();
 
 var app = builder.Build();
 
